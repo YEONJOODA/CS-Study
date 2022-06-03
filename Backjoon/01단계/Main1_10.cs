924757a34f629d89e4210d149d1590f41a7a5ae9
duswn0018@naver.com <duswn0018@naver.com>
2022-06-03 17:18:41 +0900

    퀴즈

---
+++ Backjoon/01단계/Main1_10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon._01단계
+{
+    internal class Main_10
+    {
+        static void Main(string[] args)
+        {
+            string message1 = 
+            string message2 = 
+
+            Console.WriteLine(message1 + "??!");
+            Console.WriteLine(message2 + "??!");
+        }
+    }
+}
