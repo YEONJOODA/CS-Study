5f90686a429798f157949aa8e8bac3e5f9a1b5c0
duswn0018@naver.com <duswn0018@naver.com>
2022-06-07 16:13:05 +0900

    2022/06/07 수업예제

---
+++ Ch04/4_MethodStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04
+{
+    internal class _4_MethodStack
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int n1 = 1;
+            int n2 = 2;     
+        }
+
+    }
+}
