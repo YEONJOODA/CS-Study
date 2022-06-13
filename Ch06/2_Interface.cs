23a0e63b1bd9ffde19cfcb096d8d554a42163e23
duswn0018@naver.com <duswn0018@naver.com>
2022-06-13 11:14:04 +0900

    2022/06/13 수업예제

---
+++ Ch06/2_Interface.cs
@@ -0,0 +1,37 @@
+﻿using Ch06.Sub2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06
+{
+    internal class _2_Interface
+    {
+        static void Main(string[] args)
+        {
+            // 인터페이스로 공동 클래스 구조설계
+            LGRemocon      lg      = new LGRemocon();
+            SamsungRemocon samsung = new SamsungRemocon();
+
+            lg.PowerOn();
+            lg.PowerOff();
+            lg.SoundUp();
+            lg.SoundDown();
+
+            samsung.PowerOn();
+            samsung.ChUp();
+            samsung.SoundDown();
+            samsung.PowerOff();
+
+            // 인터페이스를 활용한 다중 상속
+            TV tv = new TV();
+            tv.PowerOn();
+            tv.booting();
+            tv.Access();
+
+
+        }
+    }
+}
---
+++ Ch06/Sub2/TV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06.Sub2
+{
+    internal class TV : Internet, IComputer
+    {
+        public void PowerOn()
+        {
+            Console.WriteLine("TV PowerOn...");
+        }
+
+        public void booting()
+        {
+            Console.WriteLine("TV Booting...");
+        }
+
+        public void AccessInternet()
+        {
+            base.Access();
+        }
+    }
+}
